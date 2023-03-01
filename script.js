0a56a5058cb69d8eea3928a7cb5b9c595baa7e15
Frog <artisangiorgio98@gmail.com>
2023-03-01 22:17:30 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let isHorizontal = false
+
+
+function startAnimation(){
+    const elements = document.querySelectorAll('.menu-bar');
+    
+    elements.forEach((element) => {
+      if(isHorizontal) element.classList.remove('menu-horizontal');
+      else element.classList.add('menu-horizontal');
+    });
+    isHorizontal = !isHorizontal
+}
