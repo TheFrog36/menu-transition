2bf22226b9c8a137edccd6373ca91fac1201206b
TheFrog36 <artisangiorgio98@gmail.com>
2023-03-17 00:02:24 +0100

    pain but transparent holes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let isHorizontal = false
 
 
 function startAnimation(){
-    const elements = document.querySelectorAll('.menu-bar');
+    const elements = document.querySelectorAll('.menu-bar-container');
     
     elements.forEach((element) => {
       if(isHorizontal) element.classList.remove('menu-horizontal');
